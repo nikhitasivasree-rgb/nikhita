4458beb953e1a0773eb13d2599b06eb1984d2baf
nikhitasivasree-rgb <nikhita.sivasree@gmail.com>
2026-04-06 22:09:38 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,23 @@
 
+# G-quadruplex pattern
+pattern = r"G{3,}[ATGC]{1,7}G{3,}[ATGC]{1,7}G{3,}[ATGC]{1,7}G{3,}"
+
+matches = re.findall(pattern, sequence)
+
+# Sequence length
+length = len(sequence)
+
+# GC Content
+gc_count = sequence.count("G") + sequence.count("C")
+gc_content = (gc_count / length) * 100 if length > 0 else 0
+
+print("\n--- Analysis Result ---")
+print("Sequence Length:", length)
+print("GC Content: {:.2f}%".format(gc_content))
+
+if matches:
+    print("\nG-Quadruplex Motifs Found:")
+    for i, m in enumerate(matches, 1):
+        print(f"{i}. {m}")
+else:
+    print("\nNo G-Quadruplex motifs found.")
